7a0c726c7e86db2a08de6ddb6ac00b4280e2a666
Ahmad Ateya <ahmad.m.ateya@gmail.com>
2021-12-07 01:56:53 +0200

    adding the link package

--- sitemap-builder/main.go
+++ sitemap-builder/main.go
@@ -1,12 +1,11 @@
 package main
 
-import "flag"
-
-func main()  {
-	urlFlag := flag.String("url", "https://gophercises.com", "the url that you want to build a sitemap for")
-	flag.Parse()
-
-}
+import (
+	"flag"
+	"fmt"
+	"sitemap-builder/link"
+	"net/http"
+)
 
 /*
    1. GET the webpage
@@ -16,3 +15,21 @@ func main()  {
    5. Find all pages (BFS)
    6. print out XML
 */
+
+func main() {
+	urlFlag := flag.String("url", "https://gophercises.com", "the url that you want to build a sitemap for")
+	flag.Parse()
+
+	resp, err := http.Get(*urlFlag)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	/*
+		io.Copy.copy data from reader to a writer
+		os.Stdout is the writer that writes to the terminal
+	*/
+	links, _ := link.Parse(resp.Body)
+	fmt.Println(links)
+}
